70c3d011a6b90052ecfd26b048765fcf9b1af912
EricDavinsen <edavinsen@gmail.com>
2022-10-27 14:40:19 +0700

    GD8_C_0796

--- app/src/main/java/com/example/gd8_c_0796/ModelMain.kt
+++ app/src/main/java/com/example/gd8_c_0796/ModelMain.kt
@@ -1,8 +1,8 @@
 package com.example.gd8_c_0796
 
 class ModelMain {
-    lateinit var  strName: String
-    lateinit var strVicinity: String
-    var latloc = 0.0
-    var longloc = 0.0
+    lateinit var strName:String
+    lateinit var strVicinity:String
+    var latLoc=0.0
+    var longLoc=0.0
 }
